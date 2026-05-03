bcce7120f5713fa869fa170bdd35b3acdc214d43
kabadigital <fleursdesaba@gmail.com>
2026-05-03 04:39:50 +0200

    modification

---
+++ createAdmin.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+// 🔥 Mets ton MongoDB Atlas ici
+mongoose.connect("MONGO_URI=mongodb://kabauserpro:mouhamed90@ac-hzn0byr-shard-00-00.pa1y1ut.mongodb.net:27017,ac-hzn0byr-shard-00-01.pa1y1ut.mongodb.net:27017,ac-hzn0byr-shard-00-02.pa1y1ut.mongodb.net:27017/kaba?ssl=true&replicaSet=atlas-138rz7-shard-0&authSource=admin&retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true
+});
+
+// petit modèle Agent (simplifié pour le script)
+const agentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  role: String
+});
+
+const Agent = mongoose.model("Agent", agentSchema);
+
+async function run() {
+  try {
+    const hash = await bcrypt.hash("admin123", 10);
+
+    await Agent.create({
+      name: "Admin",
+      email: "admin@kaba.digital",
+      password: hash,
+      role: "admin"
+    });
+
+    console.log("✅ Admin créé avec succès");
+    process.exit();
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
